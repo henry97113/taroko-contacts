7191d2f25352f57deb35b7fd0f6437963d48ad65
Henry Chou <38032437+henry97113@users.noreply.github.com>
2023-11-28 02:17:20 +0800

    Allow ContactList to be sorted by last name

--- src/app/contacts/_components/ContactList/ContactList.tsx
+++ src/app/contacts/_components/ContactList/ContactList.tsx
@@ -1,31 +1,71 @@
 "use client";
 
 import * as React from "react";
-import { ArrowDownAZ } from "lucide-react";
+import { ArrowDownAZ, ArrowDownZA } from "lucide-react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 import { type Contact as ContactType } from "@/helpers/contacts";
 import Button from "@/components/Button";
 import Container from "@/components/Container";
+import VisuallyHidden from "@/components/VisuallyHidden";
 
 import styles from "./ContactList.module.css";
 import Contact from "../Contact";
+
+type SortDirection = "asc" | "desc";
 
 type ContactListProps = {
   contacts: ContactType[];
 };
 
 function ContactList({ contacts }: ContactListProps) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams()!;
+
+  const sortDir = (searchParams.get("sort") ?? "asc") as SortDirection;
+
+  const sortedContacts = React.useMemo(
+    () =>
+      [...contacts].sort((a, b) => {
+        if (sortDir === "asc") {
+          return a.last_name.localeCompare(b.last_name);
+        }
+        return b.last_name.localeCompare(a.last_name);
+      }),
+    [contacts, sortDir],
+  );
+
+  const createQueryString = React.useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams);
+      params.set(name, value);
+
+      return params.toString();
+    },
+    [searchParams],
+  );
+
   return (
     <Container>
       <div className={styles.wrapper}>
         <div className={styles.header}>
           <h2>Contacts</h2>
-          <Button size="icon">
-            <ArrowDownAZ />
+          <Button
+            size="icon"
+            title="Sort by last name"
+            onClick={() => {
+              const nextDir = sortDir === "asc" ? "desc" : "asc";
+              // <pathname>?sort=asc
+              router.push(pathname + "?" + createQueryString("sort", nextDir));
+            }}
+          >
+            {sortDir === "asc" ? <ArrowDownAZ /> : <ArrowDownZA />}
+            <VisuallyHidden>Sort by last name</VisuallyHidden>
           </Button>
         </div>
         <div className={styles["contact-wrapper"]}>
-          {contacts.map((contact) => (
+          {sortedContacts.map((contact) => (
             <Contact key={contact.id} contact={contact} />
           ))}
         </div>
