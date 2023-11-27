da1b938cea0ae4e25a978a600cbb8561956774b2
Henry Chou <38032437+henry97113@users.noreply.github.com>
2023-11-27 20:12:15 +0800

    Add Dialog UI component

---
+++ src/components/VisuallyHidden/VisuallyHidden.tsx
@@ -0,0 +1,7 @@
+import * as VisuallyHiddenPrimitive from "@radix-ui/react-visually-hidden";
+
+function VisuallyHidden(props: VisuallyHiddenPrimitive.VisuallyHiddenProps) {
+  return <VisuallyHiddenPrimitive.Root {...props} />;
+}
+
+export default VisuallyHidden;
