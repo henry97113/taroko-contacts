d3030ca5e73e06da246a3ad1dd59f5d2ecab8522
Henry Chou <38032437+henry97113@users.noreply.github.com>
2023-11-28 01:22:36 +0800

    Add ContactList header

--- src/app/contacts/_components/ContactList/ContactList.tsx
+++ src/app/contacts/_components/ContactList/ContactList.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import * as React from "react";
+import { ArrowDownAZ } from "lucide-react";
 
 import { type Contact as ContactType } from "@/helpers/contacts";
+import Button from "@/components/Button";
+import Container from "@/components/Container";
 
 import styles from "./ContactList.module.css";
-import Container from "@/components/Container";
 import Contact from "../Contact";
 
 type ContactListProps = {
@@ -16,9 +18,17 @@ function ContactList({ contacts }: ContactListProps) {
   return (
     <Container>
       <div className={styles.wrapper}>
-        {contacts.map((contact) => (
-          <Contact key={contact.id} contact={contact} />
-        ))}
+        <div className={styles.header}>
+          <h2>Contacts</h2>
+          <Button size="icon">
+            <ArrowDownAZ />
+          </Button>
+        </div>
+        <div className={styles["contact-wrapper"]}>
+          {contacts.map((contact) => (
+            <Contact key={contact.id} contact={contact} />
+          ))}
+        </div>
       </div>
     </Container>
   );
