d77a6352f05a728534cf343fe0083990bef13f10
Henry Chou <38032437+henry97113@users.noreply.github.com>
2023-11-27 22:34:26 +0800

    Force dynamic rendering on contacts page

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,6 +1,8 @@
 import { getContacts } from "@/helpers/contacts";
 import ContactList from "./_components/ContactList";
 
+export const dynamic = "force-dynamic";
+
 export default async function Contacts() {
   const contacts = await getContacts();
 
