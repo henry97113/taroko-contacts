e974f67ddfddb696667b4c234e97b0cdf4094ee0
Henry Chou <38032437+henry97113@users.noreply.github.com>
2023-11-29 13:11:14 +0800

    Remove unused contacts data

--- src/helpers/contacts.ts
+++ src/helpers/contacts.ts
@@ -2,6 +2,7 @@ import ky from "ky";
 import { z } from "zod";
 
 import { env } from "@/env";
+import { delay } from "@/utils";
 
 const baseUrl = env.NEXT_PUBLIC_API_BASE_URL;
 
@@ -17,45 +18,8 @@ const contactsSchema = contactSchema.array();
 
 export type Contact = z.infer<typeof contactSchema>;
 
-export const contactsData: z.infer<typeof contactsSchema> = [
-  {
-    id: 1,
-    first_name: "Luke",
-    last_name: "Skywalker",
-    job: "Jedi knight",
-    description: "Son of Anakin Skywalker",
-  },
-  {
-    id: 2,
-    first_name: "Obi-Wan",
-    last_name: "Kenobi",
-    job: "Jedi master",
-    description: "Old Ben was trained by Qui-Gon Jinn",
-  },
-  {
-    id: 3,
-    first_name: "Han",
-    last_name: "Solo",
-    job: "Smuggler",
-    description: "Partnered with a famous Wookie",
-  },
-  {
-    id: 4,
-    first_name: "Leia",
-    last_name: "Organa",
-    job: "Princess",
-    description: "Luke's secret twin sister",
-  },
-  {
-    id: 5,
-    first_name: "Darth",
-    last_name: "Vader",
-    job: "Sith lord",
-    description: "I am your father!",
-  },
-];
-
 export async function getContacts() {
+  await delay(2000);
   const { data } = await ky
     .get(`${baseUrl}/contacts`)
     .json<{ data: unknown }>();
